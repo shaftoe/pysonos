ba7acb28a90064efa862cd426cb1dddea6c654dc
Alexander Fortin <alexander.fortin@gmail.com>
2016-06-16 14:52:36 +0200

    wip

--- sonos.py
+++ sonos.py
@@ -3,6 +3,16 @@ from bottle import route, run
 from slackclient import SlackClient
 
 json = '''[{
+    "fallback": "Required plain-text summary of the attachment.",
+
+    "color": "#36a64f",
+
+    "pretext": "Optional text that appears above the attachment block",
+
+    "author_name": "Bobby Tables",
+    "author_link": "http://flickr.com/bobby/",
+    "author_icon": "http://flickr.com/icons/bobby.jpg",
+
     "title": "Slack API Documentation",
     "title_link": "https://api.slack.com/",
 
