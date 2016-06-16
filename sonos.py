c092e3e6b700d7a3e43e4917d763fe096105d361
Alexander Fortin <alexander.fortin@gmail.com>
2016-06-16 14:55:17 +0200

    wip

--- sonos.py
+++ sonos.py
@@ -7,16 +7,11 @@ json = '''[{
 
     "color": "#36a64f",
 
-    "pretext": "Optional text that appears above the attachment block",
-
-    "author_name": "Bobby Tables",
+    "author_name": "Anarchy monkey",
     "author_link": "http://flickr.com/bobby/",
-    "author_icon": "http://flickr.com/icons/bobby.jpg",
 
     "title": "Slack API Documentation",
     "title_link": "https://api.slack.com/",
-
-    "text": "Optional text that appears within the attachment",
 
     "fields": [
         {
