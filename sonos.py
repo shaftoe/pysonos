262042d6454d5f3ca907092ecf1eb2e27e71204e
Alexander Fortin <alexander.fortin@gmail.com>
2016-06-16 14:51:23 +0200

    wip

--- sonos.py
+++ sonos.py
@@ -3,35 +3,11 @@ from bottle import route, run
 from slackclient import SlackClient
 
 json = '''[{
-    "fallback": "Required plain-text summary of the attachment.",
-
-    "color": "#36a64f",
-
-    "pretext": "Optional text that appears above the attachment block",
-
-    "author_name": "Bobby Tables",
-    "author_link": "http://flickr.com/bobby/",
-    "author_icon": "http://flickr.com/icons/bobby.jpg",
-
     "title": "Slack API Documentation",
     "title_link": "https://api.slack.com/",
 
-    "text": "Optional text that appears within the attachment",
-
-    "fields": [
-        {
-            "title": "Priority",
-            "value": "High",
-            "short": false
-        }
-    ],
-
-    "image_url": "http://my-website.com/path/to/image.jpg",
+    "image_url": "http://popcrush1057.com/files/2013/07/369_40932711230_9148_n.jpg",
     "thumb_url": "http://example.com/path/to/thumb.png",
-
-    "footer": "Slack API",
-    "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-    "ts": 123456789
 }]'''
 
 
