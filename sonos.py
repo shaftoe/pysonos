e6624e6ab1a7c0479b85df10597923d07a269382
Alexander Fortin <alexander.fortin@gmail.com>
2016-06-16 14:47:25 +0200

    wip

--- sonos.py
+++ sonos.py
@@ -2,19 +2,52 @@
 from bottle import route, run
 from slackclient import SlackClient
 
+json = '''[{
+    "fallback": "Required plain-text summary of the attachment.",
 
-@route('/talk')
+    "color": "#36a64f",
+
+    "pretext": "Optional text that appears above the attachment block",
+
+    "author_name": "Bobby Tables",
+    "author_link": "http://flickr.com/bobby/",
+    "author_icon": "http://flickr.com/icons/bobby.jpg",
+
+    "title": "Slack API Documentation",
+    "title_link": "https://api.slack.com/",
+
+    "text": "Optional text that appears within the attachment",
+
+    "fields": [
+        {
+            "title": "Priority",
+            "value": "High",
+            "short": false
+        }
+    ],
+
+    "image_url": "http://my-website.com/path/to/image.jpg",
+    "thumb_url": "http://example.com/path/to/thumb.png",
+
+    "footer": "Slack API",
+    "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+    "ts": 123456789
+}]'''
+
+
+@route('/test')
 def talk_to_slack(message='kabooom'):
     token = "xoxp-51486140640-51539345538-51521267030-30b33fdd8a"      # found at https://api.slack.com/web#authentication
     sc = SlackClient(token)
     print sc.api_call(
         "chat.postMessage", channel="#general", text=message,
-        username='alex', icon_emoji=':o)'
+        username='ANARCHYMONKEY', attachment=json
     )
 
 
 @route('/destroyawsinstance')
 def destroyawsinstance():
+    # get_random_instance()
     return 'will terminated random instance i-12345'
 
 
